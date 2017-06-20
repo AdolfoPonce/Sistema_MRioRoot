05cfea66a372f08b53e24b6908492b2c26911bf1
adolfo <emmanueldx98@gmail.com>
2017-06-20 00:12:03 -0400

    Commit 2

---
+++ app/src/main/java/com/example/adolfo/sistema_m/launcher.java
@@ -0,0 +1,29 @@
+package com.example.adolfo.sistema_m;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by adolfo on 20/06/17.
+ */
+
+public class launcher extends AppCompatActivity{
+    public int DURACION_SPLASH =3000;
+    @Override
+            public void onCreate(Bundle b){
+        super.onCreate(b);
+        setContentView(R.layout.splash);
+
+        new Handler().postDelayed(new Runnable(){
+            public void run(){
+                // Cuando pasen los 3 segundos, pasamos a la actividad principal de la aplicación
+                Intent intent = new Intent(launcher.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            };
+        }, DURACION_SPLASH);
+    }
+
+}
